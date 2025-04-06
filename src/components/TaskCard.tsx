a49043797fc637fdffa41ce6991730cf6610ad7e
Süleyman Korcan ÜNALAN <103763684+skunalan@users.noreply.github.com>
2025-04-06 22:33:01 +0300

    Delete button added

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -5,9 +5,16 @@ import { Button, Card } from "react-bootstrap";
 interface TaskCardParams {
   task: TaskParams;
   index: number;
+  remove: (id: string) => void;
 }
 
-function TaskCard({ task, index }: TaskCardParams) {
+function TaskCard({ task, index, remove }: TaskCardParams) {
+  const handleDelete = () => {
+    if (window.confirm('Are you sure you want to delete this task?')) {
+      remove(task.id);
+    }
+  };
+
   return (
     <Draggable draggableId={task.id} index={index}>
       {(provided) => (
@@ -19,13 +26,11 @@ function TaskCard({ task, index }: TaskCardParams) {
         >
           <Card.Header className="fw-bolder">{task.title}</Card.Header>
           <Card.Body>
-            <Card.Text>
-              {task.description}
-            </Card.Text>
-            <Card.Subtitle>
-              Personel: {task.assignee}
-            </Card.Subtitle>
-            <Button className="mt-2" variant="primary">Düzenle</Button>
+            <Card.Text>{task.description}</Card.Text>
+            <Card.Subtitle>Personel: {task.assignee}</Card.Subtitle>
+            <Button className="mt-2" variant="danger" onClick={handleDelete}>
+              Delete
+            </Button>
           </Card.Body>
         </Card>
       )}
